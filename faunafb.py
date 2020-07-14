385a5e31209aeed878f168e5a0655ffe32d3657c
zvodd <zv.odd.101@gmail.com>
2020-07-14 21:57:37 +0000

    simple faunadb setup

---
+++ faunafb.py
@@ -0,0 +1,17 @@
+from faunadb import query as q
+from faunadb.objects import Ref
+from faunadb.client import FaunaClient
+from dotenv import dotenv_values
+from munch import AutoMunch
+from pprint import pprint
+
+cfg = AutoMunch(dotenv_values())
+
+# pprint(cfg)
+# exit()
+
+fdb = FaunaClient(secret=cfg.FAUNASECRET)
+
+if __name__ == "__MAIN__":
+    indexes = fdb.query(q.paginate(q.collections()))
+    pprint(indexes)
